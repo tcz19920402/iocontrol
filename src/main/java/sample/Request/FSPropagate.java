5d0d8765fea3432c5860c05809783adc85a93cc7
tcz19920402 <tcz19920402@gmail.com>
2016-10-17 23:12:53 -0500

    no message

--- src/main/java/sample/Request/FSPropagate.java
+++ src/main/java/sample/Request/FSPropagate.java
@@ -9,7 +9,7 @@ import req.StaticTree;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-
+			
 public class FSPropagate{
 	public static String printLine(List list){
 		String lstring=list.toString();
